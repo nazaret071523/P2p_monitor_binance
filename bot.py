7a0a96c60acba0b53c51b536daca5b6e97ec1815
nazaret071523 <81106602+nazaret071523@users.noreply.github.com>
2026-09-08 17:12:08 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2413,7 +2413,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=False,
-    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
